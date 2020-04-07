e896665e552264e22bd6f8b611351c0ee2d02c75
ogihara-ryo <ogihara.ryo.b@gmail.com>
2020-04-07 21:46:33 +0900

    Fetching Data for Pages

--- pages/p/[id].js
+++ pages/p/[id].js
@@ -1,15 +1,22 @@
-import { useRouter } from 'next/router';
 import Layout from '../../components/MyLayout';
+import fetch from 'isomorphic-unfetch';
 
-const Page = () => {
-  const router = useRouter();
+const Post = props => (
+  <Layout>
+    <h1>{props.show.name}</h1>
+    <p>{props.show.summary.replace(/<[/]?[pb]>/g, '')}</p>
+    {props.show.image ? <img src={props.show.image.medium} /> : null}
+  </Layout>
+);
 
-  return (
-    <Layout>
-      <h1>{router.query.title}</h1>
-      <p>This is the blog post content.</p>
-    </Layout>
-  );
+Post.getInitialProps = async function(context) {
+  const { id } = context.query;
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`);
+  const show = await res.json();
+
+  console.log(`Fetched show: ${show.name}`);
+
+  return { show };
 };
 
-export default Page;
+export default Post;
