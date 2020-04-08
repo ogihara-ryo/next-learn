4e1e0db8353b04a2191729df69c2ff92c9bae1a6
ogihara-ryo <ogihara.ryo.b@gmail.com>
2020-04-09 01:13:26 +0900

    Fetching API Routes

--- pages/index.js
+++ pages/index.js
@@ -1,58 +1,41 @@
-import Layout from '../components/MyLayout';
-import Link from 'next/link';
+import useSWR from 'swr';
 
-function getPosts() {
-  return [
-    { id: 'hello-nextjs', title: 'Hello Next.js' },
-    { id: 'learn-nextjs', title: 'Learn Next.js is awesome' },
-    { id: 'deploy-nextjs', title: 'Deploy apps with ZEIT' }
-  ];
+function fetcher(url) {
+  return fetch(url).then(r => r.json());
 }
 
-const PostLink = ({ post }) => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${post.id}`}>
-      <a>{post.title}</a>
-    </Link>
+export default function Index() {
+  const { data, error } = useSWR('/api/randomQuote', fetcher);
+  const author = data?.author;
+  let quote = data?.quote;
 
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-      
-      a {
-        text-decoration: none;
-        color: blue;
-      }
-      
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
-);
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
 
-export default function Blog() {
   return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        {getPosts().map(post =>(
-          <PostLink key={post.id} post={post} />
-        ))}
-      </ul>
+    <main className="center">
+      <div className="quote">{quote}</div>
+      {author && <span className="author">- {author}</span>}
 
       <style jsx>{`
-        h1,
-        a {
-          font-family: 'Arial';
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
         }
-        
-        ul {
-          padding: 0;
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
         }
       `}</style>
-    </Layout>
+    </main>
   );
 }
